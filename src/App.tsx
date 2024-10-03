39140d091e7be8716709c20b3814d02c8f7a99cd
Abd-Alm <avoided.sy@.com>
2024-10-03 19:30:29 +0300

    Refactor: Change the folder name, HeadPage -> PageHead

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import { Grid, GridItem, HStack } from "@chakra-ui/react";
 import { useState } from "react";
-import NavBar from "./components/HeadPage/NavBar";
+import NavBar from "./components/PageHead/NavBar";
 import NewsGrid from "./components/NewsCard/NewsGrid";
-import TopicsSelector from "./components/HeadPage/TopicsSelector";
-import SearchNews from "./components/HeadPage/SearchNews";
-import PageLabel from "./components/HeadPage/PageLabel";
+import TopicsSelector from "./components/PageHead/TopicsSelector";
+import SearchNews from "./components/PageHead/SearchNews";
+import PageLabel from "./components/PageHead/PageLabel";
 
 export interface NewsQuery {
   topic: string | "genre";
