58910129e1b0a5fe8223812099937eb4fa3e293e
Abd-Alm <avoided.sy@.com>
2024-10-04 22:34:17 +0300

    Refactor: solve the logo color in dark mode

--- src/components/HeadPage/NavBar.tsx
+++ src/components/HeadPage/NavBar.tsx
@@ -1,7 +1,14 @@
-import { Box, Center, Image } from "@chakra-ui/react";
+import { Box, Center, DarkMode, Image, useColorMode } from "@chakra-ui/react";
 import logo from "../../assets/Logo.webp";
+import LogoWhite from "../../assets/LogoWhite.webp";
 
 const NavBar = () => {
+  const { colorMode } = useColorMode();
+
+  const sizes = {
+    h: "35px",
+    w: "132px",
+  };
   return (
     <Box
       p={2}
@@ -11,7 +18,11 @@ const NavBar = () => {
       borderBottomWidth={1}
     >
       <Center>
-        <Image src={logo} h="35px" w="132px" />
+        {colorMode === "dark" ? (
+          <Image src={LogoWhite} w={sizes.w} h={sizes.h} />
+        ) : (
+          <Image src={logo} w={sizes.w} h={sizes.h} />
+        )}
       </Center>
     </Box>
   );
