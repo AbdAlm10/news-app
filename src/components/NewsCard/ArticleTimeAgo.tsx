39729d97d51d5b1017315fba7c99cc5f232f68fe
Abd-Alm <avoided.sy@.com>
2024-10-13 18:56:13 +0300

    Refactor- create a custom hook for (TimeAgo) text

--- src/components/NewsCard/ArticleTimeAgo.tsx
+++ src/components/NewsCard/ArticleTimeAgo.tsx
@@ -1,5 +1,6 @@
 import { Text, useColorMode } from "@chakra-ui/react";
 import { Article } from "../../hooks/useNews";
+import useTimeAgo from "../../hooks/useTimeAgo";
 
 interface Props {
   article: Article;
@@ -7,35 +8,7 @@ interface Props {
 
 const ArticleTimeAgo = ({ article }: Props) => {
   const { colorMode } = useColorMode();
-  const fullArticleTime: string = article.publishedAt;
-
-  // Parse the article's full time into a Date object
-  const articleDate: Date = new Date(fullArticleTime);
-
-  // Get the current date and time
-  const currentDate: Date = new Date();
-
-  // Calculate the difference in milliseconds between the current time and the article's publish time
-  const timeDifferenceMs: number =
-    currentDate.getTime() - articleDate.getTime(); // Using getTime() for clarity
-
-  // Convert the time difference to seconds, minutes, hours, and days
-  const timeDifferenceMinutes: number = Math.floor(
-    timeDifferenceMs / (1000 * 60)
-  );
-  const timeDifferenceHours: number = Math.floor(timeDifferenceMinutes / 60);
-  const timeDifferenceDays: number = Math.floor(timeDifferenceHours / 24);
-  const timeDifferenceMonths: number = Math.floor(timeDifferenceDays / 30); // Approximation of months
-
-  // Determine the appropriate format to display
-  const timeAgo: string =
-    timeDifferenceMinutes < 60
-      ? `${timeDifferenceMinutes} M ago`
-      : timeDifferenceHours < 24
-      ? `${timeDifferenceHours} H ago`
-      : timeDifferenceDays < 30
-      ? `${timeDifferenceDays} D ago`
-      : `${timeDifferenceMonths} M ago`;
+  const timeAgo = useTimeAgo(article.publishedAt);
 
   return (
     <Text
---
+++ src/hooks/useTimeAgo.ts
@@ -0,0 +1,29 @@
+import { useMemo } from "react";
+
+const useTimeAgo = (fullTime: string) => {
+  return useMemo(() => {
+    const articleDate: Date = new Date(fullTime);
+    const currentDate: Date = new Date();
+    const timeDifferenceMs: number =
+      currentDate.getTime() - articleDate.getTime();
+
+    const timeDifferenceMinutes: number = Math.floor(
+      timeDifferenceMs / (1000 * 60)
+    );
+    const timeDifferenceHours: number = Math.floor(timeDifferenceMinutes / 60);
+    const timeDifferenceDays: number = Math.floor(timeDifferenceHours / 24);
+    const timeDifferenceMonths: number = Math.floor(timeDifferenceDays / 30); // Approximation
+
+    if (timeDifferenceMinutes < 60) {
+      return `${timeDifferenceMinutes} M ago`;
+    } else if (timeDifferenceHours < 24) {
+      return `${timeDifferenceHours} H ago`;
+    } else if (timeDifferenceDays < 30) {
+      return `${timeDifferenceDays} D ago`;
+    } else {
+      return `${timeDifferenceMonths} M ago`;
+    }
+  }, [fullTime]);
+};
+
+export default useTimeAgo;
